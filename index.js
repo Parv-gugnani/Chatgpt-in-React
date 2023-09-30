110f71dc55f8bbf64aa8e1bcf44b06d250203575
Parv-gugnani <9765.parv@gmail.com>
2023-09-30 18:40:24 +0530

    node

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-import { Configuration, OpenAIApi } from "openai";
+const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
+
 const configuration = new Configuration({
-  organization: "org-3FoXI7L3tfnx24EGgLu55SoN",
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+
+async function callApi() {
+  try {
+    console.log(completion.data.choices[0].text);
+  } catch (error) {
+    console.error(
+      "Error:",
+      error.response ? error.response.data : error.message
+    );
+  }
+}
+
+//create
+
+const app = express();
+const port = 3000;
+
+app.post("/", async (req, res) => {
+  const completion = await openai.createCompletion({
+    engine: "text-davinci-003",
+    prompt: "Say this is a test.",
+    max_tokens: 7,
+    temperature: 0,
+  });
+  console.log(response.data.choices[0].text);
+  res.json({
+    data: response.data,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`example app listening at http://localhost:${port}`);
+});
