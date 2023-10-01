daba280ed1b19cc3bed356b1d35cf8ad8362a67e
Parv-gugnani <9765.parv@gmail.com>
2023-10-01 13:06:47 +0530

    completed

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
+require("dotenv").config();
 
-const configuration = new Configuration({
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+
+// async function callApi() {
+//   try {
+//     console.log(completion.data.choices[0].text);
+//   } catch (error) {
+//     console.error(
+//       "Error:",
+//       error.response ? error.response.data : error.message
+//     );
+//   }
+// }
+const OpenAI = require("openai");
+
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
 
-async function callApi() {
-  try {
-    console.log(completion.data.choices[0].text);
-  } catch (error) {
-    console.error(
-      "Error:",
-      error.response ? error.response.data : error.message
-    );
-  }
-}
+const callApi = async () => {
+  const chatCompletion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Hello!" }],
+    max_tokens: 30, //this limits reposne from openai , saves , just for the test purposes
+  });
+  console.log(chatCompletion.choices[0].message);
+};
 
+callApi();
+//
+// /
+//
+//
+//
 //create
 
 const app = express();
